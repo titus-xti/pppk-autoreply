4294e7d27f2e39af78d69c37e1a9ad1ad393554b
Titus.Prasetyo@xapiens.id <Titus.Prasetyo@xapiens.id>
2025-08-28 14:19:58 +0700

    remove new line

--- chats.go
+++ chats.go
@@ -1,56 +1,58 @@
 package main
 
 var (
-	OpeningGreeting = `Selamat datang di Asisten Virtual PPPK GKJP:\n
-	1. Info Pemilihan\n
-	2. Registrasi pemilihan Online\n
-	3. Kirim ulang surat suara Digital\n\n
+	OpeningGreeting = `Selamat datang di Asisten Virtual PPPK GKJP:
+	1. Info Pemilihan
+	2. Registrasi pemilihan Online
+	3. Kirim ulang surat suara Digital
 	Balas dengan angka: 1, 2 atau 3.`
 
 	ErrorSaving = `Maaf, terjadi kesalahan saat menyimpan data. Silakan coba lagi.`
 
-	NotRegistered = `Nomor anda belum terdaftar\n
+	NotRegistered = `Nomor anda belum terdaftar
 	Silahkan menghubungi Majelis Wilayah atau Panitia`
 
 	ErrorQuerying = `Maaf, terjadi kesalahan saat memeriksa data. Silakan coba lagi.`
-	ErrorWilayah  = `Wilayah pelayanan tidak valid. Silahkan isi pp1/pp2/serpong/bukit/reni\n\n`
-	ErrorDOB      = `Tahun lahir tidak valid. Silahkan isi tahun lahir 4 digit\n\n`
+	ErrorWilayah  = `Wilayah pelayanan tidak valid. Silahkan isi pp1/pp2/serpong/bukit/reni`
+	ErrorDOB      = `Tahun lahir tidak valid. Silahkan isi tahun lahir 4 digit`
 
-	SuccessRegister = `Terima kasih!\n
-	Pendaftaran pemilihan online berhasil dengan data sebagai berikut:\n
-	Nama: %s\n
-	Wilayah: %s\n
-	Tahun Lahir: %s\n\n
-	Surat suara Digital: https://pemilihan.gkj-pamulang.org/%s\n\n
-	Silahkan surat suara Digital ini dengan aman.\n\n
+	SuccessRegister = `Terima kasih!
+	Pendaftaran pemilihan online berhasil dengan data sebagai berikut:
+	Nama: %s
+	Wilayah: %s
+	Tahun Lahir: %s
+	Surat suara Digital: https://pemilihan.gkj-pamulang.org/%s
+	Silahkan surat suara Digital ini dengan aman.
 	Ketik '0' untuk kembali ke menu.`
 
-	RegistrationHelp = `%sUntuk mendaftar pemilihan online, silahkan kirim pesan dengan format:\n
-	DAFTAR-<nama lengkap jemaat>-<wilayah pelayanan>-<tahun lahir># \n\n
-	Tahun Lahir 4 Digit, Contoh: 1985\n
-	Wilayah pp1/pp2/serpong/bukit/reni\n\n
-	Contoh:\n
-	DAFTAR-James Munthe-pp1-1972#\n
-	DAFTAR-Maria Fatmitasari-pp2-1972#\n
-	DAFTAR-Ery Setiawan-bukit-1972#\n
-	DAFTAR-Florencia Irena-reni-1980#\n
+	RegistrationHelp = `%sUntuk mendaftar pemilihan online, silahkan kirim pesan dengan format:
+	DAFTAR-<nama lengkap jemaat>-<wilayah pelayanan>-<tahun lahir># 
+	Tahun Lahir 4 Digit, Contoh: 1985
+	Wilayah pp1/pp2/serpong/bukit/reni
+	Contoh:
+	DAFTAR-James Munthe-pp1-1972#
+	DAFTAR-Maria Fatmitasari-pp2-1972#
+	DAFTAR-Ery Setiawan-bukit-1972#
+	DAFTAR-Florencia Irena-reni-1980#
 	DAFTAR-Titus Adi Prasetyo-serpong-1985#`
 
-	InfoPemilihanHelp = `%sPemilihan pendeta di laksanakan secara Onsite dan Online \n
-	Pemilihan Online di Tanggal 28 September 2025\n
-	Pemilihan Onsite di Tanggal 28 September 2025\n
-	Untuk informasi lebih lanjut, silahkan menghubungi Majelis Wilayah\n
+	InfoPemilihanHelp = `%sPemilihan pendeta di laksanakan secara Onsite dan Online 
+	Pemilihan Online di Tanggal 28 September 2025
+	Pemilihan Onsite di Tanggal 28 September 2025
+	Untuk informasi lebih lanjut, silahkan menghubungi Majelis Wilayah
 	dan Panitia di nomor 081297898399`
 
-	ResendVoteHelp = `%sBerikut ini surat suara Digital Anda : \n\n`
+	ResendVoteHelp = `%sBerikut ini surat suara Digital Anda : `
 
-	AlreadyRegistered = `Anda sudah terdaftar sebelumnya dengan data:\n
-	Nama: %s\n
-	Wilayah: %s\n
-	Tahun Lahir: %s\n\n
-	Data tidak dapat diubah. \n\nHubungi panitia di nomor 081297898399 jika ada kesalahan data.`
+	AlreadyRegistered = `Anda sudah terdaftar sebelumnya dengan data:
+	Nama: %s
+	Wilayah: %s
+	Tahun Lahir: %s
+	
+	Data tidak dapat diubah. 
+	Hubungi panitia di nomor jika ada kesalahan data.`
 )
 
 const (
-	backHint = "\n\nketik 0 untuk kembali ke menu utama"
+	backHint = "ketik 0 untuk kembali ke menu utama"
 )
