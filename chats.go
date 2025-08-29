121ba906f4e8c3b6238a21e0cac2552452b1422e
Titus.Prasetyo@xapiens.id <Titus.Prasetyo@xapiens.id>
2025-08-29 15:53:55 +0700

    Perbaikan redaksi informasi

--- chats.go
+++ chats.go
@@ -1,17 +1,18 @@
 package main
 
 var (
-	OpeningGreeting = `Selamat datang di Asisten Virtual PPPK GKJP:
+	OpeningGreeting = `Selamat datang di Chatbot Panitia Pemanggilan Pendeta Kedua GKJ Pamulang:
 
-1. Info Pemilihan
-2. Registrasi pemilihan Online
+1. Informasi Pemilihan
+2. Registrasi Pemilihan Online
 3. Kirim ulang surat suara Digital
 
 Balas dengan angka: 1, 2 atau 3.`
 
 	ErrorSaving = `Maaf, terjadi kesalahan saat menyimpan data. Silakan coba lagi.`
 
-	NotRegistered = `Nomor anda belum terdaftar
+	NotRegistered = `Nomor anda belum terdaftar,
+Jika anda adalah warga dewasa GKJ Pamulang,
 Silahkan menghubungi Majelis Wilayah atau Panitia`
 
 	ErrorQuerying = `Maaf, terjadi kesalahan saat memeriksa data. Silakan coba lagi.`
@@ -39,8 +40,18 @@ Ketik "ya" untuk mendaftar pemilihan online`
 
 	InfoPemilihanHelp = `%sPemilihan pendeta di laksanakan secara Onsite dan Online
 
-1. Pemilihan Onsite di Tanggal 28 September 2025 Jam 10:00 WIB (Setelah Ibadah) sampai Selesai
-2. Pemilihan Online di Tanggal 25 September 2025 Jam 00:00 WIB sampai 27 September 2025 Jam 24:00 WIB
+1. Pemilihan Onsite/Langsung 
+- Dilaksanakan pada Tanggal 28 September 2025 Jam 10:00 WIB (Setelah Ibadah) sampai Selesai
+- Jemaat di harapkan hadir di Gereja, 30 menit sebelum Ibadah dimulai untuk mengisi daftar hadir dan mendapatkan surat suara
+- Jemaat melakukan pencoblosan kartu suara dengan pilihan SETUJU atau TIDAK SETUJU
+
+2. Pemilihan Online 
+- Diutamakan bagi Jemaat yang sedang Study luar kota, Tugas di luar Kota atau berhalangan hadir di Gereja pada saat pemilihan Onsite/Langsung Tanggal 28 September 2025 
+- Jemaat terlebih dahulu melakukan pendaftaran pemilihan online, paling lambat 21 September 2025
+- Satu nomor HP/WA untuk satu Jemaat
+- Pastikan surat suara digital yang diterima disimpan dengan baik, dan tidak di pergunakan orang lain
+- Jemaat bisa mulai memilih pada Tanggal 25 September 2025 Jam 00:00 WIB sampai 27 September 2025 Jam 24:00 WIB
+- Jemaat melakukan memilih pada kartu suara digital dengan pilihan SETUJU atau TIDAK SETUJU
 
 Untuk informasi lebih lanjut, 
 silahkan menghubungi Majelis Wilayah atau Panitia`
@@ -49,20 +60,22 @@ silahkan menghubungi Majelis Wilayah atau Panitia`
 
 https://pemilihan.gkj-pamulang.org/%s
 
-Simpan surat suara digital ini dengan aman.`
+Simpan surat suara digital ini dengan aman,
+Pastikan surat suara digital ini tidak dipergunakan orang lain`
 
 	NotYetRegistered = `Anda belum terdaftar di dalam pemilihan online,
 Silahkan melakukan pendaftaran terlebih dahulu`
 
-	AlreadyRegistered = `Anda sudah terdaftar sebelumnya dengan data:
+	AlreadyRegistered = `Anda sudah terdaftar dengan data sebagai berikut:
+
 Nama: %s
 Wilayah: %s
 Tahun Lahir: %s
 
 Data tidak dapat diubah. 
-Hubungi panitia jika ada kesalahan data.`
+Hubungi Majelis wilayah atau Panitia jika ada kesalahan data.`
 )
 
 const (
-	backHint = "\n\nKetik 0 untuk kembali ke menu utama"
+	backHint = "\n\nKetik 0 untuk kembali ke menu utama\n\nSemua pesan di jawab oleh System\nMohon hanya mengirimkan pesan sesuai instruksi\n\nTerimakasih"
 )
